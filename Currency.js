c62b0db6e782d109d43d8820d5b640b0d4322a6b
Prasoon Gupta <prasoongupta961@gmail.com>
2025-09-08 19:11:26 +0530

    Add files via upload

---
+++ Currency.js
@@ -0,0 +1,61 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let option = document.createElement("option");
+    option.value = currCode;
+    option.innerText = currCode;
+
+    if (select.name === "from" && currCode === "USD") {
+      option.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      option.selected = "selected";
+    }
+    select.append(option);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  
+  const URL = `${BASE_URL}/${fromCurr.value}`;
+
+  let response = await fetch(URL);
+  let data = await response.json();
+
+  if (data.result !== "success") {
+    msg.innerText = "Error fetching exchange rates!";
+    return;
+  }
+
+  let rate = data.rates[toCurr.value];
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+});
